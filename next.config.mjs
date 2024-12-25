a02aca9ff814aca6289a03232c6d0552746478e7
Lucien Leseigle <ggenesum@gmail.com>
2024-12-25 21:36:07 +0100

    Fix: Update next.config.mjs for deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  
+  // Webpack customization for `node-fetch`
+  webpack: (config) => {
+    config.externals = {
+      ...config.externals,
+      'node-fetch': 'commonjs node-fetch',
+    };
+    return config;
+  },
+
+  // Optionally disable ESLint during builds
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
